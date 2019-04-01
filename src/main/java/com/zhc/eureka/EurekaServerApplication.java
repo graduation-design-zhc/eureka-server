2ecfcf372bdc49fbf655f185549cf3a176344b67
张华厂 <zhanghuachang@yupaopao.cn>
2019-04-01 18:59:12 +0800

    init

---
+++ src/main/java/com/zhc/eureka/EurekaServerApplication.java
@@ -0,0 +1,19 @@
+package com.zhc.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+        System.out.println(new SimpleDateFormat("[yyyy-MM-dd HH:mm:ss]").format(new Date()) + "eureka-server started!");
+    }
+
+}
